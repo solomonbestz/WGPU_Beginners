97fe39b528dc9a4cf88d1eb115b9adbb0a60059b
solomonbestz <kingsolodawarrior@gmail.com>
2026-02-13 19:44:39 +0100

    feats: Basic window, up and running

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use glfw::{Action, Context, Key};
+use glfw::{Action, Context, Key, fail_on_errors};
 
 fn main() {
-    // let mut glfw = glfw::init(fail)
-}
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
 
-// flatpak install flathub com.github.wwmm.easyeffects
+    let (mut window, events) = glfw.create_window(800, 600, "WGPU Begineers", glfw::WindowMode::Windowed).unwrap();
+
+    window.make_current();
+
+    while !window.should_close() {
+        glfw.poll_events();
+        window.swap_buffers();
+    }
+}
