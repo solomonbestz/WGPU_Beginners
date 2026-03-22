99b9cafaf8a401fa787f0b957db517f0edf5f358
solomonbestz <kingsolodawarrior@gmail.com>
2026-03-22 22:48:18 +0100

    feat: turned the main function to run function

--- src/main.rs
+++ src/main.rs
@@ -143,7 +143,7 @@ impl<'a> State<'a>{
 
 // Continue to work on the renderer function
 
-fn main() {
+fn run() {
     let mut glfw = glfw::init(fail_on_errors!()).unwrap();
 
     let (mut window, events) = glfw.create_window(800, 600, "WGPU Begineers", glfw::WindowMode::Windowed).unwrap();
@@ -168,4 +168,9 @@ fn main() {
         }
         window.swap_buffers();
     }
+}
+
+
+fn main(){
+    
 }
